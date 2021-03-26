b917004e36b50df3efcdc61e2f0e869afa21cba1
cart0uche <hafid.saou@gmail.com>
2021-03-26 18:22:30 +0100

    display address account

--- react_time_capsule/src/components/PageHeader.js
+++ react_time_capsule/src/components/PageHeader.js
@@ -1,36 +1,47 @@
-import React, { useState, useEffect } from "react"
-import {Menu, Button} from 'semantic-ui-react'
-import Web3 from 'web3'
+import React, { useState, useEffect } from "react";
+import { Menu, Button } from "semantic-ui-react";
+import Web3 from "web3";
 
-export default () =>{
+export default () => {
+  const [accounts, setAccounts] = useState([]);
+  const [once, setOnce] = useState(true);
 
-	const [accounts, setAccounts] = useState([]);
+  useEffect(async () => {
+    if (once) {
+	  const _accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      setAccounts(_accounts);
+      setOnce(false);
+    }
+  }, [once]);
 
-	const isMetaMaskConnected = () => accounts && accounts.length > 0
+  async function onConnectWallet(event) {
+    if (typeof window.ethereum !== "undefined") {
+      console.log("MetaMask is installed!");
+    }
 
-	async function onConnectWallet(event) {
-		if (typeof window.ethereum !== 'undefined') {
-			console.log('MetaMask is installed!');
-		  }
-		  
-		const _accounts = await  window.ethereum.request({ method: 'eth_requestAccounts' });
-		setAccounts(_accounts);
-		console.log(_accounts)
-	}
+    const _accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccounts(_accounts);
+  }
 
-	return (
-		<Menu style={{ marginTop:'10px' }}>
+  function renderMetamask() {
+    if (accounts && accounts.length > 0) {
+      return <a className="item">{accounts[0]}</a>;
+    } else {
+      return (
+        <Button floated="right" color="blue" onClick={onConnectWallet}>
+          Connect wallet
+        </Button>
+      );
+    }
+  }
 
-    		<a className="item">
-				Ethereum Time Caspsule
-			</a>
+  return (
+    <Menu style={{ marginTop: "10px" }}>
+      <a className="item">Ethereum Time Caspsule</a>
 
-		<Menu.Menu position="right">
-
-			<Button floated="right" color='blue' onClick={onConnectWallet}>Connect wallet</Button>
-
-		</Menu.Menu>
-
-		</Menu>
-	);
-}
+      <Menu.Menu position="right">{renderMetamask()}</Menu.Menu>
+    </Menu>
+  );
+};
